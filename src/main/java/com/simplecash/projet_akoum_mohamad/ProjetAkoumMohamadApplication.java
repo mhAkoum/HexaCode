756342b9614a9a4f2eda2103f6891e2f622cac31
mohmmad akoum <the.akoum@Akoums-mac.local>
2025-11-28 09:59:48 +0100

    chore: initialize Spring Boot project

---
+++ src/main/java/com/simplecash/projet_akoum_mohamad/ProjetAkoumMohamadApplication.java
@@ -0,0 +1,13 @@
+package com.simplecash.projet_akoum_mohamad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAkoumMohamadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetAkoumMohamadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simplecash/projet_akoum_mohamad/ProjetAkoumMohamadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplecash.projet_akoum_mohamad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAkoumMohamadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
